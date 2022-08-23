66e10abf2a0f51d0836fb81f622abc1274858a78
Gabriel Silva <sr.silvagabriel@gmail.com>
2022-08-22 23:05:45 -0300

    feature: list all users

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,18 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+    console.log(user, user_id);
+    if (!user) {
+      throw new Error("User not found.");
+    }
+
+    if (!user.admin) {
+      throw new Error("User must be admin.");
+    }
+
+    const all = this.usersRepository.list();
+    return all;
   }
 }
 
